fdbf477c89024480e3d323327de7e2e2baa8eb4f
Parallels <parallels@kali-linux-2021-3.localdomain>
2023-09-09 12:14:43 +0200

    using big numbers...TODO: let's generate another big num and compute the requested operation

--- rand/es4.c
+++ rand/es4.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <string.h>
-#include <inttypes.h>
 #include <openssl/rand.h>
+#include <openssl/bn.h>
+
 
 #define SIZE 4
 
 int main() {
-    unsigned char random1[SIZE];
+    unsigned char random1[SIZE], formatted[SIZE+2];
+    BIGNUM *bn1 = BN_new();
 
+    /*
     if (RAND_load_file("/dev/random", SIZE)!= SIZE){
         fprintf(stderr, "Error initializing the PRNG.\n");
     }
-    RAND_bytes(random1, SIZE);
-    
-    for(int i = 0 ; i < SIZE; i++) {
-        printf("%2x", random1[i]);
-    }
-    
-    uint32_t number1 = *(uint32_t*)random1;
-    printf("\nCasting 4 bytes into integer: %"PRIu32"\n", number1);
+    RAND_bytes(random1, SIZE);*/
+
+    BN_rand(bn1, 32, -1, 1);
+
+    BN_print_fp(stdout, bn1);
+    printf("\n");
 
     return 0;
 }
